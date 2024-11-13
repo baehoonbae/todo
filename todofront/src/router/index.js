9da62586cee7840ad32c102e3673c56056a4b465
baehoonbae <129490755+baehoonbae@users.noreply.github.com>
2024-11-13 23:52:18 +0900

    chore: Import 수정

--- todofront/src/router/index.js
+++ todofront/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import TodoCalendar from '../components/TodoCalendar.vue';
-import TodoBigCalendar from '@/components/TodoBigCalendar.vue';
-import CategoryRegist from '@/views/CategoryRegist.vue';
-import TodoHome from '@/views/TodoHome.vue';
-import UserLogin from '@/views/UserLogin.vue';
-import UserSignUp from '@/views/UserSignUp.vue';
-import TodoMy from '@/views/TodoMy.vue';  
-import CategoryList from '@/views/CategoryList.vue';
-import CategoryUpdate from '@/views/CategoryUpdate.vue';
+import TodoCalendar from '../components/Calendar.vue';
+import TodoBigCalendar from '@/components/BigCalendar.vue';
+import CategoryRegist from '@/views/CategoryRegistView.vue';
+import TodoHome from '@/views/HomeView.vue';
+import UserLogin from '@/views/UserLoginView.vue';
+import UserSignUp from '@/views/UserSignUpView.vue';
+import TodoMy from '@/views/MyView.vue';  
+import CategoryList from '@/views/CategoryListView.vue';
+import CategoryUpdate from '@/views/CategoryUpdateView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
